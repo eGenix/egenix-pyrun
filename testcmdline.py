94ceffe368f193dfc00020e0c67f510b1af2dd50
Marc-Andre Lemburg <mal@egenix.com>
2013-04-10 10:36:44 +0000

    Turn the command line interface test into a real test script.
    See #1076.

--- testcmdline.py
+++ testcmdline.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 #
-# Test the pyrun cmd line interface
+# Test the pyrun cmd line interface.
 #
-import os, sys, subprocess
+# Note: This test currently only works on Unix platforms and then only
+#       for Python 2.7. See #1076.
+#
+import os, sys, subprocess, re
 
 PYRUN = 'pyrun'
 PYTHON = 'python2.7'
@@ -15,37 +18,102 @@ def run(command):
                             stderr=subprocess.PIPE)
     return pipe.stdout.read() + pipe.stderr.read()
 
+def match_result(result, pattern):
+
+    m = re.match(pattern.replace('[0]', '\[0\]'), result)
+    if m is None:
+        print ('*** Result  %r does not match\n'
+               '    pattern %r' % (result, pattern))
+    return m
+
 def test_cmd_line(runtime=PYRUN):
 
     os.chdir(TESTDIR)
 
+    l = []
     result = run('%s hello.py' % runtime)
-    print repr(result)
-    assert result == 'Hello world !\n'
+    assert match_result(
+        result,
+        'Hello world !\n'
+        )
+    l.append(result)
     
     result = run('%s main.py' % runtime)
-    print repr(result)
-    #assert result == 'Hello world !\n'
+    assert match_result(
+        result,
+        "__file__: 'main.py'\n"
+        "__name__: '__main__'\n"
+        "__path__: not defined\n"
+        "sys.path[0]: '%s'\n" % os.path.abspath(os.getcwd())
+        )
+    l.append(result)
     
     result = run('%s pkg/main.py' % runtime)
-    print repr(result)
+    assert match_result(
+        result,
+        "__file__: 'pkg/main.py'\n"
+        "__name__: '__main__'\n"
+        "__path__: not defined\n"
+        "sys.path[0]: '.*/pkg'\n"
+        )
+    l.append(result)
 
     result = run('%s pkg/sub/main.py' % runtime)
-    print repr(result)
+    assert match_result(
+        result,
+        "__file__: 'pkg/sub/main.py'\n"
+        "__name__: '__main__'\n"
+        "__path__: not defined\n"
+        "sys.path[0]: '.*/pkg/sub'\n"
+        )
+    l.append(result)
     
     result = run('%s dir' % runtime)
-    print repr(result)
+    assert match_result(
+        result,
+        "__file__: '.*/dir/__main__.py'\n"
+        "__name__: '__main__'\n"
+        "__path__: not defined\n"
+        "sys.path[0]: 'dir'\n"
+        )
+    l.append(result)
     
     result = run('%s zipfiles.zip' % runtime)
-    print repr(result)
+    assert match_result(
+        result,
+        "__file__: 'zipfiles.zip/__main__.py'\n"
+        "__name__: '__main__'\n"
+        "__path__: not defined\n"
+        "sys.path[0]: 'zipfiles.zip'\n"
+        )
+    l.append(result)
     
     result = run('%s zipfiles.abc' % runtime)
-    print repr(result)
+    assert match_result(
+        result,
+        "__file__: 'zipfiles.abc/__main__.py'\n"
+        "__name__: '__main__'\n"
+        "__path__: not defined\n"
+        "sys.path[0]: 'zipfiles.abc'\n"
+        )
+    l.append(result)
     
     result = run('%s zipdir.zip' % runtime)
-    print repr(result)
+    assert match_result(
+        result,
+        ".*: can't find '__main__' module in 'zipdir.zip'\n"
+        )
+    l.append(result)
+
+    return l
     
 ###
 
 if __name__ == '__main__':
-    test_cmd_line(sys.argv[1])
+    try:
+        runtime = sys.argv[1]
+    except IndexError:
+        print '%s <abs path to runtime>' % sys.argv[0]
+        sys.exit(1)
+    test_cmd_line(runtime)
+    print '%s passes all command line tests' % runtime
