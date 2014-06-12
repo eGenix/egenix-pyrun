cbef14e24cf2e1d315393fce3222627ee65de2f2
Marc-Andre Lemburg <mal@egenix.com>
2014-06-12 15:57:43 +0000

    Reenable the testcmdline.py tests for Python 3.4.
    See #1383 and #1003.

--- testcmdline.py
+++ testcmdline.py
@@ -76,12 +76,8 @@ def test_cmd_line(runtime=PYRUN):
         )
     l.append(result)
     
-    # These features are only supported in pyrun 2.7.
-    #
-    # XXX They currently don't work in pyrun 3.4, even though they
-    # should. See #1383.
-    #
-    if python_version(runtime) == '2.7':
+    # These features are only supported in pyrun 2.7 and 3.4+.
+    if python_version(runtime) >= '2.7':
         result = run('%s -S dir' % runtime)
         assert match_result(
             result,
