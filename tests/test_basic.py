171cc9e4290ac4b8d602ed6381170cb839bb6b2e
Marc-Andre Lemburg <mal@egenix.com>
2024-06-24 20:55:55 +0200

    Test importing more modules
    Test all 3.11 Setup based modules
    
    Disable mxDateTime test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -49,6 +49,7 @@ try:
     import array
     import binascii
     import bz2
+    import pickle
     if PYTHON_VERSION < (3, 0):
         # Removed/replaced in Python 3
         import cPickle
@@ -63,6 +64,7 @@ try:
     import mmap
     import _bisect
     import _ssl
+    import _sqlite3
 except ImportError as reason:
     print('%s not found.' % module_name(reason))
     if PYTHON_VERSION >= (2, 5):
@@ -98,6 +100,103 @@ except ImportError as reason:
 else:
     print('done.')
 
+print('Try loading a few Python 3 stdlib modules...')
+try:
+    import _sha3
+    import html.parser
+    import urllib.parse
+    import typing
+    if PYTHON_VERSION >= (3, 11):
+        import tomllib
+except ImportError as reason:
+    print('%s not found.' % module_name(reason))
+    if PYTHON_VERSION >= (3, 7):
+        sys.exit(1)
+else:
+    print('done.')
+
+# Check imports of all modules installed via Setup
+
+# These are all modules built for Python 3.11
+setup_modules = [
+    '_asyncio',
+    '_bisect',
+    '_blake2',
+    '_bz2',
+    '_codecs_cn',
+    '_codecs_hk',
+    '_codecs_iso2022',
+    '_codecs_jp',
+    '_codecs_kr',
+    '_codecs_tw',
+    '_contextvars',
+    '_crypt',
+    '_csv',
+    '_ctypes',
+    '_curses',
+    '_curses_panel',
+    '_datetime',
+    '_decimal',
+    '_elementtree',
+    '_hashlib',
+    '_heapq',
+    '_json',
+    '_lsprof',
+    '_md5',
+    '_multibytecodec',
+    '_multiprocessing',
+    '_opcode',
+    '_pickle',
+    '_posixshmem',
+    '_posixsubprocess',
+    '_queue',
+    '_random',
+    '_sha1',
+    '_sha256',
+    '_sha3',
+    '_sha512',
+    '_socket',
+    '_sqlite3',
+    '_ssl',
+    '_statistics',
+    '_struct',
+    '_typing',
+    '_uuid',
+    #'_xxsubinterpreters', # not included in PyRun
+    '_zoneinfo',
+    'array',
+    'audioop',
+    'binascii',
+    'cmath',
+    'fcntl',
+    'grp',
+    'math',
+    'mmap',
+    'ossaudiodev',
+    'pyexpat',
+    'readline',
+    'resource',
+    'select',
+    'spwd',
+    'syslog',
+    'termios',
+    'unicodedata',
+    'zlib',
+]
+# Note: These modules were not built due to missing deps:
+# _dbm                  _gdbm                 _lzma
+# _tkinter              nis
+
+print('Try loading Setup compiled Python 3.11 stdlib modules...')
+for modname in setup_modules:
+    try:
+        __import__(modname)
+    except ImportError as reason:
+        print('%s not loaded: %s' % (modname, reason))
+        if PYTHON_VERSION >= (3, 11):
+            sys.exit(1)
+print('done.')
+
 print('Try loading ctypes (a shared mod)...')
 try:
     import _ctypes
@@ -106,13 +205,13 @@ except ImportError:
 else:
     print('done.')
 
-print('Try loading mxDateTime (a shared mod)...')
-try:
-    import mx.DateTime
-except ImportError:
-    print('mxDateTime not found.')
-else:
-    print('done.')
+# print('Try loading mxDateTime (a shared mod)...')
+# try:
+#     import mx.DateTime
+# except ImportError:
+#     print('mxDateTime not found.')
+# else:
+#     print('done.')
 
 print('')
 print('Loaded modules after test run:')
